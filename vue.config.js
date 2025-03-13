e8f0dc5e09c7852e0d454ed5c651e8ecf92f85f0
Fox Tail <foxtail25@gmail.com>
2025-03-13 13:40:29 +0300

    vue.conf

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-module.exports = {
-	// publicPath: process.env.NODE_ENV === 'production'
-	// 	? '/code_mu_proj_bookmark_storage_service/'
-	// 	: '/'
-	publicPath: '/code_mu_proj_bookmark_storage_service/'
-
-}
+// module.exports = {
+// 	publicPath: process.env.NODE_ENV === 'production'
+// 		? '/code_mu_proj_bookmark_storage_service/'
+// 		: '/'
+// }
