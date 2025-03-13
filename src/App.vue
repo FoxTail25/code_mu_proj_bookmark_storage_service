c12e2f352736a9ddcafee73429530537ec132baf
Fox Tail <foxtail25@gmail.com>
2025-03-13 11:15:11 +0300

    back

--- src/App.vue
+++ src/App.vue
@@ -23,11 +23,10 @@ store.setFirstData()
 
     <ul class="nav nav-tabs justify-content-center mb-4">
       <li class="nav-item">
-        <RouterLink to="/code_mu_proj_bookmark_storage_service/" class="nav-link">Мои закладки</RouterLink>
+        <RouterLink to="/" class="nav-link">Мои закладки</RouterLink>
       </li>
       <li class="nav-item">
-        <RouterLink to="/code_mu_proj_bookmark_storage_service/edit_group" class="nav-link">Редактирование групп
-        </RouterLink>
+        <RouterLink to="/edit_group" class="nav-link">Редактирование групп</RouterLink>
       </li>
       <li class="nav-item">
         <RouterLink to="/edit_record" class="nav-link">Редактирование записей</RouterLink>
