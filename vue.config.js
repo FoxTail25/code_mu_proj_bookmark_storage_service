0bc4ade52273d8589e1a4341e8112bbb1abc980c
Fox Tail <foxtail25@gmail.com>
2025-03-13 11:32:40 +0300

    может теперь....

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
-		? '/code_mu_proj_bookmark_storage_service/'
-		: '/'
+	// publicPath: process.env.NODE_ENV === 'production'
+	// 	? '/code_mu_proj_bookmark_storage_service/'
+	// 	: '/'
+	publicPath: '/code_mu_proj_bookmark_storage_service/'
+
 }
