34f5dfafa1942b1f85f0fcf96db9732a7bd26581
Fox Tail <foxtail25@gmail.com>
2025-03-13 12:17:50 +0300

    repit2

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/code_mu_proj_bookmark_storage_service/#/',
+      path: '/',
       name: 'home',
       component: HomeView,
     },
