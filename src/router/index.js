aed08a8cb5b1a4fe697550ffc8654c8bdfbf9699
Fox Tail <foxtail25@gmail.com>
2025-03-13 11:07:42 +0300

    Попытка исправить баг с роутингом на GitHub

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView,
+//     },
+//     {
+//       path: '/edit_group',
+//       name: 'edit_group',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/EditGroupView.vue'),
+//     },
+//     {
+//       path: '/edit_record',
+//       name: 'edit_record',
+//       component: () => import('../views/EditRecordView.vue'),
+//     },
+//   ],
+// })
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: ' /code_mu_proj_bookmark_storage_service/',
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/edit_group',
+      path: ' /code_mu_proj_bookmark_storage_service/edit_group',
       name: 'edit_group',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
@@ -18,7 +41,7 @@ const router = createRouter({
       component: () => import('../views/EditGroupView.vue'),
     },
     {
-      path: '/edit_record',
+      path: ' /code_mu_proj_bookmark_storage_service/edit_record',
       name: 'edit_record',
       component: () => import('../views/EditRecordView.vue'),
     },
