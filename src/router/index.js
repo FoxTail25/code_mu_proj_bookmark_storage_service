fca8ac9e3227203016523a993b1efd9c870d2ac3
Fox Tail <foxtail25@gmail.com>
2025-03-13 16:13:25 +0300

    вроде нашел решение с роутером.

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/edit_group',
-      name: 'edit_group',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/EditGroupView.vue'),
-    },
-    {
-      path: '/edit_record',
-      name: 'edit_record',
-      component: () => import('../views/EditRecordView.vue'),
-    },
-  ],
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView,
+//     },
+//     {
+//       path: '/edit_group',
+//       name: 'edit_group',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/EditGroupView.vue'),
+//     },
+//     {
+//       path: '/edit_record',
+//       name: 'edit_record',
+//       component: () => import('../views/EditRecordView.vue'),
+//     },
+//   ],
+// })
+
+// export default router
+
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import HomeView from '@/views/HomeView.vue'
+import EditGroupView from '@/views/EditGroupView.vue'
+import EditRecordView from '@/views/EditRecordView.vue'
+
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/edit_group', component: EditGroupView },
+  { path: '/edit_record', component: EditRecordView }
+]
+
+export const router = createRouter({
+  history: createMemoryHistory(),
+  routes
 })
-
-export default router
